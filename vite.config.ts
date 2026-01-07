22f50ebc34878c980e9c1b0a9a3c78c2a4dd53a6
SweetConsole <sweetconsole@gmail.com>
2026-01-08 02:36:08 +0300

    Fix Error and Add chunks

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,14 @@ import react from "@vitejs/plugin-react"
 export default defineConfig({
 	plugins: [react()],
 	build: {
-		ssr: true
+		rollupOptions: {
+			output: {
+				manualChunks(id: string) {
+					if (id.includes("node_modules")) {
+						return "plugins"
+					}
+				}
+			}
+		}
 	}
 })
